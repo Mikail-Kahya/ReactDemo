4834f732f0c34710c8964e748971c4b58d24390f
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2026-03-19 17:29:53 +0100

    feat: list that adds items

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-//import { useState } from 'react'
-//import heroImg from './assets/hero.png'
+import { useState, type SubmitEventHandler } from 'react'
 import './reset.css'
 import './style.css'
 
+type Task = { id : string, 
+  name : string, 
+  isCompleted: boolean,
+  createdTime : Date
+}
+
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState<Task[]>([])
+
+  function handleSubmit(e : React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const newTask : Task = { 
+      id: crypto.randomUUID(), 
+      name: newItem, 
+      isCompleted: false, 
+      createdTime: new Date()
+    }
+
+    setTodos((currentTodos => {
+        return [...currentTodos, newTask];
+    }));
+
+    setNewItem("");
+  }
+
   return <>
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="task">New item</label>
-        <input type="text" name="task" id="task" />
+        <input type="text" value={newItem} onChange={e => setNewItem(e.target.value)} name="task" id="task" />
       </div>
       <button className="btn">Add</button>
-    </form>;
+    </form>
     <h1 className="header">Todo List</h1>
     <ul className='list'>
-      <li>
-        <label>
-          <input type="checkbox"/>
-          item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
+      { todos.map(todo => {
+        return (
+          <li key={todo.id}>
+            <label>
+              <input  type="checkbox" 
+                      checked={todo.isCompleted} 
+                      onChange={(e : HTMLInputElement)=> toggleTodo(todo.id, e.formTarget.checked)}/>
+              {todo.name}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        );
+      }) }
+      
     </ul>
   </> 
 }
