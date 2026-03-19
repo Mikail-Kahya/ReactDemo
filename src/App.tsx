261a891311bd914ca597325689364433dfee05d7
Mikail Kahya <138559177+Mikail-Kahya@users.noreply.github.com>
2026-03-19 16:54:25 +0100

    add basic list

--- src/App.tsx
+++ src/App.tsx
@@ -4,5 +4,23 @@ import './reset.css'
 import './style.css'
 
 export default function App() {
-  return "hi";
+  return <>
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="task">New item</label>
+        <input type="text" name="task" id="task" />
+      </div>
+      <button className="btn">Add</button>
+    </form>;
+    <h1 className="header">Todo List</h1>
+    <ul className='list'>
+      <li>
+        <label>
+          <input type="checkbox"/>
+          item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+  </> 
 }
